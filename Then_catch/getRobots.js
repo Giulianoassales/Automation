7334a1b202b9b079bd2d1f2e87185e5bfc2fe9b1
Giulianoassales <giuliano.na.94@gmail.com>
2023-04-19 12:23:11 -0300

    prueba

---
+++ Then_catch/getRobots.js
@@ -0,0 +1,16 @@
+const axios = require('axios');
+
+axios.get('https://swapi.dev/api/people/')
+  .then((response) => {
+    all = response.data.results
+  })
+  .then(()=>{ 
+    count = Object.keys(all).length
+    
+    for (let i = 0; i<count; i++){
+        if (all[i].name === "C-3PO" || all[i].name === "R2-D2"){
+            console.log("Nombre: " + all[i].name)
+            console.log("Altura: " + all[i].height)
+            console.log("Sexo: " + all[i].gender)
+        }    
+    }})
